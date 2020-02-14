9cf693c3284767e25aa24390a2fe6d0c44355752
omoshthegeek254 <omoshthegeek@gmail.com>
2020-02-14 15:29:37 +0300

    added the firebas app

--- app/src/main/java/com/example/movie101/adapters/MoviesListAdapter.java
+++ app/src/main/java/com/example/movie101/adapters/MoviesListAdapter.java
@@ -1,6 +1,7 @@
 package com.example.movie101.adapters;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,12 +13,15 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.movie101.R;
 import com.example.movie101.models.Result;
 import com.example.movie101.ui.MoviesActivity;
+import com.squareup.picasso.Picasso;
 
 
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+
+import static com.example.movie101.utilities.Constants.POSTER_BASE_URL;
 
 public class MoviesListAdapter extends RecyclerView.Adapter<MoviesListAdapter.MoviesViewHolder> {
         private List<Result> mMovies;
@@ -52,6 +56,7 @@ public class MoviesListAdapter extends RecyclerView.Adapter<MoviesListAdapter.Mo
             @BindView(R.id.movieTitleNameTextView) TextView mTitleTextView;
             @BindView(R.id.movieSummaryTextView) TextView mOverviewTextView;
             @BindView(R.id.ratingTextView) TextView mRatingTextView;
+
             private Context mContext;
 
             public MoviesViewHolder(View itemView) {
@@ -61,9 +66,14 @@ public class MoviesListAdapter extends RecyclerView.Adapter<MoviesListAdapter.Mo
             }
 
             public void bindMovie(Result movies) {
-                mTitleTextView.setText(movies.getName());
+                //Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(mMovieImageView);
+               Picasso.get().load(POSTER_BASE_URL+movies.getPosterPath()).into(mMovieImageView);
+                Log.v("rposter path",movies.getPosterPath());
+                mTitleTextView.setText(movies.getOriginalTitle());
                 mOverviewTextView.setText(movies.getOverview());
                 mRatingTextView.setText("Rating: " + movies.getVoteAverage() + "/5");
             }
         }
     }
+
+
