405d2a89dc138c4e1eaa3c71c56a1fd78f54ac04
omoshthegeek254 <omoshthegeek@gmail.com>
2020-02-16 11:41:12 +0300

    added the logic for the registration page posing to firebase

--- app/src/main/java/com/example/movie101/ui/Register.java
+++ app/src/main/java/com/example/movie101/ui/Register.java
@@ -1,15 +1,21 @@
 package com.example.movie101.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.movie101.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
@@ -81,6 +87,23 @@ public class Register extends AppCompatActivity {
                     mPassword.setError("PassWords Don't match ,Try Again");
                     return;
                 }
+
+                //setting progressbar visibility
+                progressBarReg.setVisibility(View.VISIBLE);
+
+                //registering user
+                fbAuth.createUserWithEmailAndPassword(email,passWord).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Register.this,"User Created Successfully",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent((getApplicationContext(),MainActivity.class));
+                        }else {
+                            Toast.makeText(Register.this,"Error !!!"+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
 
             }
         });
